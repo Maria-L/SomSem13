62b9a282f20b42616bdb0358789107ecd1ad4da5
unknown <astreija@googlemail.com>
2013-05-26 13:23:05 +0200

    Datenbanken Aufgabe 2

---
+++ Datenbanken/Aufgabe 2/Merlin Tabellen erstellen.sql
@@ -0,0 +1,42 @@
+create table Voegel 
+  (VogelID integer not null PRIMARY KEY,
+  Kategorie         varchar2(10) not null,
+  Latainischer_Name varchar2(100) unique not null,
+  Englischer_Name   varchar2(50),
+  Deutscher_Name    varchar2(50),
+  check(Kategorie in ('species', 'subspecies'))
+  );
+  
+create table Beobachtungsort
+  (BeobachtungsortID integer not null PRIMARY KEY,
+  Beobachtungsort       varchar2(100) not null,
+  Staat                 varchar2(40),
+  Zoographische_Region  varchar2(50)
+  );
+  
+create table Benutzer
+  (BenutzerName         varchar2(60) not null PRIMARY KEY,
+  EMail_Addresse        varchar2(100) unique not null,
+  Rolle                 integer not null
+  );
+  
+create table Checkliste
+  (VogelID              integer not null,
+  BeobachtungsID        integer not null,
+  PRIMARY KEY(VogelID,BeobachtungsID),
+  FOREIGN KEY(VogelID)        references Voegel,
+  FOREIGN KEY(BeobachtungsID) references Beobachtungsort
+  );
+  
+create table Beobachtet
+  (BeobachtungsID     integer not null PRIMARY KEY,
+  BenutzerName        varchar2(60) not null,
+  BeobachtungsortID   integer not null,
+  VogelID             integer not null,
+  BeobBeginn          date,
+  BeobEnde            date,
+  Kommentar           clob,
+  FOREIGN KEY(BenutzerName)       references Benutzer,
+  FOREIGN KEY(BeobachtungsortID)  references Beobachtungsort,
+  FOREIGN KEY(VogelID)            references Voegel
+  );
