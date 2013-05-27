103cb9e217ba4bee5f1143c66ae91809977baa4c
unknown <astreija@googlemail.com>
2013-05-27 14:53:36 +0200

    Schema_loeschen hinzugefuegt

---
+++ Datenbanken/Aufgabe 2/Schema_loeschen.sql
@@ -0,0 +1,5 @@
+drop table beobachtet cascade constraints PURGE;
+drop table checkliste cascade constraints PURGE;
+drop table Beobachtungsort cascade constraints PURGE;
+drop table Voegel cascade constraints PURGE;
+drop table benutzer cascade constraints PURGE;
