6cbcc03c2ca3fcc049be7bff570a95dededf863c
unknown <astreija@googlemail.com>
2013-05-29 18:47:57 +0200

    RMP Aufgabe 3

--- Datenbanken/Aufgabe 2/Merlin Datenbank befüllen.sql
+++ Datenbanken/Aufgabe 2/Merlin Datenbank befüllen.sql
@@ -1,4 +1,47 @@
-/*################################################ BEGINN ############################################################*/
+/*################################################ BEGINN TABELLEN ERSTELLEN ############################################################*/
+create table Voegel
+  (VogelID integer not null PRIMARY KEY,
+  Kategorie         varchar2(20) not null,
+  Latainischer_Name varchar2(80) unique not null,
+  Englischer_Name   varchar2(70),
+  Deutscher_Name    varchar2(50), /*Ist nicht not null, weil es einen sehr viel größeren Aufwand erzeugt hätte, die Tabellen zu befüllen*/
+  check(Kategorie in ('species', 'subspecies', 'group (monotypic)', 'group (polytypic)'))
+  );
+  
+create table Beobachtungsort
+  (BeobachtungsortID integer not null PRIMARY KEY,
+  Beobachtungsort       varchar2(500) not null,
+  Staat                 varchar2(40),
+  Zoographische_Region  varchar2(50)
+  );
+  
+create table Benutzer
+  (BenutzerName         varchar2(60) not null PRIMARY KEY,
+  EMail_Addresse        varchar2(100) unique not null,
+  Rolle                 integer not null
+  );
+  
+create table Checkliste
+  (VogelID              integer not null,
+  BeobachtungsID        integer not null,
+  PRIMARY KEY(VogelID,BeobachtungsID),
+  FOREIGN KEY(VogelID)        references Voegel,
+  FOREIGN KEY(BeobachtungsID) references Beobachtungsort
+  );
+  
+create table Beobachtet
+  (BeobachtungsID     integer not null PRIMARY KEY,
+  BenutzerName        varchar2(60) not null,
+  BeobachtungsortID   integer not null,
+  VogelID             integer not null,
+  BeobBeginn          date,
+  BeobEnde            date,
+  Kommentar           clob,
+  FOREIGN KEY(BenutzerName)       references Benutzer,
+  FOREIGN KEY(BeobachtungsortID)  references Beobachtungsort,
+  FOREIGN KEY(VogelID)            references Voegel
+  );
+/*################################################ BEGINN TABELLEN BEFÜLLEN ############################################################*/
 /*Fügt alle Einträge aus MERLIN.birds nach VOEGEL*/
 INSERT INTO VOEGEL
   (VogelID, Latainischer_Name, Kategorie, englischer_name)
@@ -22,16 +65,31 @@ UPDATE VOEGEL
                                 where latainischer_name = MERLIN.BIRDS_IOC.IOC_SCIENTIFIC_NAME) 
   where (select MERLIN.BIRDS_IOC.IOC_GERMAN_NAME 
     from MERLIN.BIRDS_IOC where latainischer_name = MERLIN.BIRDS_IOC.IOC_SCIENTIFIC_NAME) is not null;
-                                
-/*Dasselbe nochmal in Englisch*/
+
+/*Schiebe die englischen Namen aus BIRDS_DE in die betreffenden Tupel aus VOEGEL, wobei bestehende Einträge nicht überschrieben werden*/
+UPDATE VOEGEL
+  SET VOEGEL.englischer_name = (select MERLIN.BIRDS_DE.de_englisch
+                                from MERLIN.BIRDS_DE
+                                where latainischer_name = MERLIN.BIRDS_DE.de_latein)
+  WHERE englischer_name is null;
+
+/*Schiebe die englischen Namen aus BIRDS_IOC in die betreffenden Tupel aus VOEGEL, wobei bestehende Einträge nicht überschrieben werden*/  
+UPDATE VOEGEL
+  SET VOEGEL.englischer_name = (select MERLIN.BIRDS_IOC.ioc_english_name
+                                from MERLIN.BIRDS_IOC
+                                where latainischer_name = MERLIN.BIRDS_IOC.ioc_scientific_name)
+  WHERE englischer_name is null;
                                 
  /*Species und dazugehörige Subspecies ohne deutschen Namen werden gelöscht*/                         
 delete from voegel where 
-    (trim(substr(latainischer_name, 1, instr(latainischer_name, ' ',1, 2))) in (select trim(latainischer_name) from voegel where deutscher_name is null and kategorie = 'species') 
+    (trim(substr(latainischer_name, 1, instr(latainischer_name, ' ',1, 2))) in (select trim(latainischer_name) 
+                                                                                from voegel 
+                                                                                where deutscher_name is null 
+                                                                                and kategorie = 'species') 
     or 
     deutscher_name is null and kategorie = 'species');
 
-/*Fülle die ranges aus BIRDS in BEOBACHTUNGSORT, wobei doppelte nicht mit kopiert werden und überflüssige b_id's gelöscht werden*/
+/*Fülle die ranges aus BIRDS in BEOBACHTUNGSORT, wobei doppelte nicht mit kopiert werden und überflüssige b_id's für das select gelöscht werden*/
 INSERT INTO BEOBACHTUNGSORT
   (beobachtungsortID, beobachtungsort)
 SELECT 
@@ -48,10 +106,10 @@ SELECT
 FROM voegel v, merlin.birds mb, beobachtungsort b where v.latainischer_name = mb.b_scientific_name 
                                                   and b.beobachtungsort = mb.b_range ;
                                                   
-/*Da nicht genug Platz in der Datenbank war um die Checkliste einzufügen haben wir noch einen View erstellt, der den Inhalt der Checkliste darstellt*/
-create or replace view checkliste_view as 
-  select vogelid, beobachtungsortid from voegel v, merlin.birds mb, beobachtungsort b 
-    where v.latainischer_name = mb.b_scientific_name 
-    and b.beobachtungsort = mb.b_range;
-select * from checkliste_view;
-/*################################################  ENDE  ############################################################*/
+/*################################################  BEGINN TABELLEN LÖSCHEN  ############################################################*/
+
+drop table beobachtet cascade constraints PURGE;
+drop table checkliste cascade constraints PURGE;
+drop table Beobachtungsort cascade constraints PURGE;
+drop table Voegel cascade constraints PURGE;
+drop table benutzer cascade constraints PURGE;
--- Datenbanken/Aufgabe 2/merlin_.sql
+++
@@ -1,4 +0,0 @@
-select vogelid, latainischer_name, deutscher_name, case 
-    when deutscher_name is null then 'delete' 
-    when deutscher_name is not null then 'keep'
-    end from voegel where kategorie = 'species';
